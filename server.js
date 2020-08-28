307da0761ab489c903f8c4b2cc08ddda277d9bce
Mazen Abdelbasir <mazen.elba@gmail.com>
2020-08-28 13:41:18 -0400

    new mods

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path"); // built-in module
 
+// Define Global Variables
 const app = express();
 
 // Heroku Step 1: Connect Backend Server (Virtual Host or Local Host at 8080)
@@ -13,9 +14,8 @@ const PORT = process.env.PORT || 8080;
 
 const routes = require("./routes/api");
 
-// "mongodb://localhost/mern2"
 // Heruko Step 2: Connect Backend Database (Virtual URI or Local Host)
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern2", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -32,6 +32,10 @@ app.use(express.urlencoded({ extended: false }));
 // Heroku Step 3: Connect Backend-to-Frontend (Import Client's BUILD Directory Into Server)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // HTTP Request Logger
